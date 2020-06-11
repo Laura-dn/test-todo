71a98cd2f61d6f0d743bcbc8beaa70d5209380bf
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-06-11 19:42:49 +0300

    update

---
+++ precache-manifest.22d36e7b20e1c62b17d7efb88d221a1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478ebb7303f27f8de4e7fe0f677d4c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a68231da729e0148a577",
+    "url": "/static/css/main.b5694f43.chunk.css"
+  },
+  {
+    "revision": "602a4d4035529635ae00",
+    "url": "/static/js/2.d6eab59d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6eab59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68231da729e0148a577",
+    "url": "/static/js/main.95d49085.chunk.js"
+  },
+  {
+    "revision": "8fd7b7025359b211ff89",
+    "url": "/static/js/runtime-main.908d2c6b.js"
+  },
+  {
+    "revision": "7814140981989c8d534aed03de7d24ae",
+    "url": "/static/media/bg.78141409.jpg"
+  }
+]);
